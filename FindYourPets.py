36146eeb168e55dd408368883e67cfe8ed31e2a8
L-Yajing <yajingli0532@hotmail.com>
2021-11-01 10:54:48 -0500

    update

--- FindYourPets.py
+++ FindYourPets.py
@@ -24,7 +24,7 @@ DROPOUT_RATE = 0.1
 LEARNING_RATE = 0.1
 DECAY_STEPS = 100
 DECAY_RATE = 0.95
-EPOCHS = 30
+EPOCHS = 1
 PATIENCE = 5
 
 base_model = '../input/keras-pretrained-models/ResNet152_Top_ImageNet.h5'
@@ -35,10 +35,10 @@ base_model.trainable = False
 
 train_csv = pd.read_csv('../input/petfinder-pawpularity-score/train.csv')
 test_csv = pd.read_csv('../input/petfinder-pawpularity-score/test.csv')
-train_no_id = train_csv.copy()
-train_no_id.pop('Id')
-corr = np.corrcoef(train_no_id,rowvar=False)
+ids = train_csv.pop('Id')
+corr = np.corrcoef(train_csv,rowvar=False)
 ax = sns.heatmap(corr)
+train_csv['Id'] = ids
 
 
 # Image data directories
@@ -81,15 +81,20 @@ def get_image(path: str) -> tf.Tensor:
 
 @tf.function
 def get_image_wscore(path: str, score: int) -> tuple:
-    """Function returns preprocessed image and label.
+    """Function loads image from a file and preprocesses it.
     :param path: Path to image file
-    :param label: Class label
-    :return: tf.Tensor with preprocessed image, numeric label
+    :param score: Pawpularity Score
+    :return: Tuple of Tensor with preprocessed image and Pawpularity score
     """
     return get_image(path), score
 
 @tf.function
 def get_dataset(x, y=None) -> tf.data.Dataset:
+    """Function returns preprocessed image and label.
+    :param x: Path to image file
+    :param y: Pawpularity score if known
+    :return: tf.Tensor with preprocessed image and numeric score.
+    """
     if y is not None:
         ds = tf.data.Dataset.from_tensor_slices((x,y))
         return ds.map(get_image_wscore, num_parallel_calls=AUTOTUNE).batch(BATCH_SIZE).prefetch(buffer_size=AUTOTUNE)
@@ -98,6 +103,8 @@ def get_dataset(x, y=None) -> tf.data.Dataset:
         return ds.map(get_image, num_parallel_calls=AUTOTUNE).batch(BATCH_SIZE).prefetch(buffer_size=AUTOTUNE)
 
 
+
+# Base model: Resnet152
 image_model_base = tf.keras.models.Sequential(
     [tf.keras.layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3)),
     tf.keras.layers.experimental.preprocessing.RandomFlip(mode='horizontal'),
@@ -117,8 +124,10 @@ image_model_base.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_sch
 image_model_base.summary()
 
 # Early Stopping Rule
-early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=PATIENCE, restore_best_weights=True)
+# It shows that 5 Epochs is enough.
+# early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=PATIENCE, restore_best_weights=True)
 
+# Split train dataset into a training subset(80%) and a validation subset(20%).
 train_subset, valid_subset = train_test_split(
     train_csv[['path', TARGET]],
     test_size=VAL_SIZE, shuffle=True)
@@ -136,14 +145,15 @@ base_pred_test = image_model_base.predict(test_ds)
 print('Prediction Completed')
 
 
+
 # XGBoost Tuning
 # min_rmse = float("Inf")
 # best_params = None
 # dtrain = xgb.DMatrix(base_pred_train, label=train_subset[TARGET])
 
-params = {'max_depth':None,
-          'min_child_weight': None,
-          'alpha':None,
+params = {'max_depth':5,
+          'min_child_weight': 6,
+          'alpha':6,
           'colsample_bytree':0.5,
           'eta':0.1
          }
@@ -154,6 +164,7 @@ params = {'max_depth':None,
 #                      for alpha in range(4,7)
 #                     ]
 
+# Find best params for XGBoost
 # for max_depth, min_child_weight, alpha in gridsearch_params:
 #     print("CV with max_depth={}, min_child_weight={}, alpha={}".format(
 #         max_depth,min_child_weight,alpha))
@@ -175,19 +186,21 @@ params = {'max_depth':None,
 
 # xgb_model = xgb.XGBRegressor(max_depth = best_params[0], min_child_weight = best_params[1], alpha = best_params[2],
 #                              colsample_bytree = params['colsample_bytree'], eta = params['eta'], n_estimators = 100)
-xgb_model = xgb.XGBRegressor(max_depth = 5, min_child_weight = 6, alpha = 6, colsample_bytree = params['colsample_bytree'],
+
+# Put the best params in the final model
+xgb_model = xgb.XGBRegressor(max_depth = params['max_depth'], min_child_weight = params['min_child_weight'],
+                             alpha = params['alpha'], colsample_bytree = params['colsample_bytree'],
                              eta = params['eta'], n_estimators = 100)
 xgb_model.fit(base_pred_train, train_subset[TARGET])
 
-# %% [code] {"execution":{"iopub.status.busy":"2021-10-31T00:56:38.992609Z","iopub.execute_input":"2021-10-31T00:56:38.993034Z","iopub.status.idle":"2021-10-31T00:58:27.054988Z","shell.execute_reply.started":"2021-10-31T00:56:38.992987Z","shell.execute_reply":"2021-10-31T00:58:27.053885Z"}}
+
 # SVR Tuning
 param_grid = {'C': [20], 'gamma': [0.8], 'kernel': ['poly','rbf','sigmoid']}
 svr_model = GridSearchCV(SVR(),param_grid, refit=True, verbose=2)
-# svr_model.fit(base_pred_train, train_subset[TARGET])
-# print(svr_model.best_estimator_)
+# Put the best params in the final model
 svr_model.fit(base_pred_train, train_subset[TARGET])
 
-# %% [code] {"execution":{"iopub.status.busy":"2021-10-31T00:58:38.294044Z","iopub.execute_input":"2021-10-31T00:58:38.294396Z","iopub.status.idle":"2021-10-31T01:29:11.787093Z","shell.execute_reply.started":"2021-10-31T00:58:38.294353Z","shell.execute_reply":"2021-10-31T01:29:11.786063Z"}}
+
 # Fit Neural Network
 nn_model = image_model_base
 nn_model.add(tf.keras.layers.BatchNormalization())
@@ -197,36 +210,34 @@ nn_model.add(tf.keras.layers.Dropout(DROPOUT_RATE, name='top_dropout2'))
 nn_model.add(tf.keras.layers.Dense(1, name='score'))
 
 # Compile the Neural Network model
+# Use MSE as Loss Function.
 nn_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
                     loss=tf.keras.losses.MeanSquaredError(),
                     metrics=[tf.keras.metrics.RootMeanSquaredError()])
 nn_model.summary()
 
 history = nn_model.fit(train_ds, validation_data = valid_ds,
-                          epochs=EPOCHS, callbacks=[early_stop],
+                          epochs=EPOCHS,# callbacks=[early_stop],
                           use_multiprocessing=True, workers=-1)
 
-# %% [code] {"execution":{"iopub.status.busy":"2021-10-31T01:29:11.792841Z","iopub.execute_input":"2021-10-31T01:29:11.793119Z","iopub.status.idle":"2021-10-31T01:35:37.841891Z","shell.execute_reply.started":"2021-10-31T01:29:11.793085Z","shell.execute_reply":"2021-10-31T01:35:37.840872Z"}}
-# Prediction based on new models.
+
+# Making Predictions based on new models for the validation dataset.
 print('Start predictions - New Models - Valid')
 nn_valid = nn_model.predict(valid_ds, use_multiprocessing=True, workers=os.cpu_count())
 svr_valid = svr_model.predict(base_pred_valid)
 xgb_valid = xgb_model.predict(base_pred_valid)
+three_pred_valid = [[x[0] for x in nn_valid],svr_valid,xgb_valid]
 print('Prediction Completed')
 
-# %% [code] {"execution":{"iopub.status.busy":"2021-10-31T01:35:37.843587Z","iopub.execute_input":"2021-10-31T01:35:37.844412Z","iopub.status.idle":"2021-10-31T01:35:37.859287Z","shell.execute_reply.started":"2021-10-31T01:35:37.844354Z","shell.execute_reply":"2021-10-31T01:35:37.858215Z"}}
-# Performance on validaty dataset - RMSE
-rsme = np.sqrt( np.mean( (valid_subset[TARGET] - np.array(nn_valid).reshape(valid_subset[TARGET].shape))**2.0 ) )
-print('NN RSME =',rsme,'\n')
-rsme = np.sqrt( np.mean( (valid_subset[TARGET] - np.array(svr_valid))**2.0 ) )
-print('SVR RSME =',rsme,'\n')
-rsme = np.sqrt( np.mean( (valid_subset[TARGET] - np.array(xgb_valid))**2.0 ) )
-print('XGB RSME =',rsme,'\n')
 
-# %% [code] {"execution":{"iopub.status.busy":"2021-10-31T01:54:03.534425Z","iopub.execute_input":"2021-10-31T01:54:03.534828Z","iopub.status.idle":"2021-10-31T01:54:03.540749Z","shell.execute_reply.started":"2021-10-31T01:54:03.534769Z","shell.execute_reply":"2021-10-31T01:54:03.540032Z"}}
-three_pred_valid = [[x[0] for x in nn_valid],svr_valid,xgb_valid]
+# Performance on validation dataset - RMSE
+rmses = list(map(lambda x: np.sqrt( np.mean( (valid_subset[TARGET] - np.array(x))**2.0 ) ),three_pred_valid))
+print('NN RSME =',rmses[0],'\n')
+print('SVR RSME =',rmses[1],'\n')
+print('XGB RSME =',rmses[2],'\n')
 
-# %% [code] {"execution":{"iopub.status.busy":"2021-10-31T01:54:05.668708Z","iopub.execute_input":"2021-10-31T01:54:05.669302Z","iopub.status.idle":"2021-10-31T01:54:05.709709Z","shell.execute_reply.started":"2021-10-31T01:54:05.669242Z","shell.execute_reply":"2021-10-31T01:54:05.708776Z"}}
+
+# Find best sets of weights with lowest rmse.
 best_weights = None
 lowest_rsme = float('Inf')
 weights = [(nn,svr)
@@ -240,23 +251,19 @@ for nn, svr in weights:
         if rmse<lowest_rsme:
             lowest_rsme = rmse
             best_weights = [nn,svr,xgb]
-        #print('Weights = ', nn, svr, xgb)
-        #print('RSME = ',rmse)
+
 print(lowest_rsme)
 print(best_weights)
 
-# %% [code] {"execution":{"iopub.status.busy":"2021-10-31T01:54:24.673158Z","iopub.execute_input":"2021-10-31T01:54:24.67349Z","iopub.status.idle":"2021-10-31T01:54:30.450052Z","shell.execute_reply.started":"2021-10-31T01:54:24.673451Z","shell.execute_reply":"2021-10-31T01:54:30.448858Z"}}
-# Prediction for test dataset
+
+# Making Predictions for the test dataset.
 print('Start predictions - New Models - Test')
 test_csv['nn_Pawpularity'] = nn_model.predict(test_ds, use_multiprocessing=True, workers=os.cpu_count())
 test_csv['svr_Pawpularity'] = svr_model.predict(base_pred_test)
 test_csv['xgb_Pawpularity'] = xgb_model.predict(base_pred_test)
 print('Prediction Completed')
 
-best_weights[0] += 2
-best_weights[1] += 2
-best_weights[2] -= 4
+# Generate Final Predictions and Save it.
 test_csv[TARGET] = np.dot(test_csv[['nn_Pawpularity','svr_Pawpularity','xgb_Pawpularity']],best_weights)/10
-test_csv
-
 test_csv[['Id', TARGET]].to_csv('submission.csv', index=False)
+test_csv
